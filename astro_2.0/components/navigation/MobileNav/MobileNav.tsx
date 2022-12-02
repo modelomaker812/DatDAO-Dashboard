0d4b027b06dfac6797dd6b90490bc780bef9413b
dreamofnikolay <happydev999@gmail.com>
2022-12-02 13:29:56 -0800

    [fix] mobile nav bar items

--- astro_2.0/components/navigation/MobileNav/MobileNav.tsx
+++ astro_2.0/components/navigation/MobileNav/MobileNav.tsx
@@ -3,11 +3,14 @@ import React, { VFC } from 'react';
 import { useWalletContext } from 'context/WalletContext';
 
 import {
-  CFC_LIBRARY_NAV_CONFIG,
-  MY_DAOS_NAV_CONFIG,
-  MY_FEED_NAV_CONFIG,
-  ASTRO_FEED_NAV_CONFIG,
-  DISCOVER_NAV_CONFIG,
+  HOME_CONFIG,
+  NFT_CONFIG,
+  MEMBERS_CONFIG,
+  BOUNTIES_CONFIG,
+  PROPOSALS_CONFIG,
+  POLLS_CONFIG,
+  TREASURY_CONFIG,
+  ORGANIZATION_CONFIG
 } from 'astro_2.0/components/navigation/navConfig';
 import { NavButton } from 'astro_2.0/components/navigation/NavButton';
 import { useDaoIds } from 'hooks/useDaoIds';
@@ -20,13 +23,26 @@ export const MobileNav: VFC = () => {
 
   const navConfig = accountId
     ? [
-        MY_FEED_NAV_CONFIG,
-        MY_DAOS_NAV_CONFIG,
-        CFC_LIBRARY_NAV_CONFIG,
-        DISCOVER_NAV_CONFIG,
-        ASTRO_FEED_NAV_CONFIG,
-      ]
-    : [DISCOVER_NAV_CONFIG, CFC_LIBRARY_NAV_CONFIG, ASTRO_FEED_NAV_CONFIG];
+      HOME_CONFIG,
+      NFT_CONFIG,
+      MEMBERS_CONFIG,
+      BOUNTIES_CONFIG,
+      PROPOSALS_CONFIG,
+      POLLS_CONFIG,
+      TREASURY_CONFIG,
+      ORGANIZATION_CONFIG
+    ]
+    // : [DISCOVER_NAV_CONFIG, CFC_LIBRARY_NAV_CONFIG, ASTRO_FEED_NAV_CONFIG];
+    : [
+      HOME_CONFIG,
+      NFT_CONFIG,
+      MEMBERS_CONFIG,
+      BOUNTIES_CONFIG,
+      PROPOSALS_CONFIG,
+      POLLS_CONFIG,
+      TREASURY_CONFIG,
+      ORGANIZATION_CONFIG
+    ]
 
   const navItems = navConfig.map(conf => {
     return (
